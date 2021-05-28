8b8e3ab02979cc3df45f893f5af3a73da0d010c9
idusansavic <54639922+idusansavic@users.noreply.github.com>
2021-05-28 19:42:21 +0200

    Upload
    Initial upload

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Dusan Savic on 02/02/2021.
+//  Copyright © 2021 Dusan Savic. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
